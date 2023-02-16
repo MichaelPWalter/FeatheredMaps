d212aacf4a2015459cb8a4063bb51dd06dcf8744
Michael Walter <104144492+MichaelPWalter@users.noreply.github.com>
2023-02-16 20:16:20 +0100

    updating Combine Tax.py

--- Taxonomy/Combine_Worldwide_Taxonomy.py
+++ Taxonomy/Combine_Worldwide_Taxonomy.py
@@ -22,8 +22,34 @@ for file in file_list:
     print(file)
     merged_df = merged_df.merge(df, on="scientificName", how="outer")
 
+# Creating a new DataFrame with rows where IUCN_Species_Code is missing
+Unmatched_df =merged_df[merged_df["IUCN_Species_Code"].isna()].copy()
+#Unmatched_df = Unmatched_df.drop(columns=[["IUCN_Species_Code"],["IUCN_common_name"],["altcommonNames"],["Synonym"]])
 
+# Removing rows where IUCN_Species_Code is missing from original_df
+merged_df.dropna(subset=["IUCN_Species_Code"], inplace=True)
+
+
+
+
+
+# Iterate over all rows in Unmatched_df
+for _, row in Unmatched_df.iterrows():
+    # Iterate over all columns that contain "_common_name" but not "IUCN_common_name"
+    for col in [c for c in merged_df.columns if '_common_name' in c and c != 'IUCN_common_name']:
+        # Check for matches between scientificName and Synonym and copy matching common names
+        mask = merged_df['Synonym'].str.contains(row['scientificName'], na=False)
+        if mask.any():
+            merged_df.loc[mask, col] = row[col]
+
+    # Only append row if no matches were found
+    if not mask.any():
+        merged_df = merged_df.append(row, ignore_index=True)
+
+
+#synonyms_df.to_csv("Taxonomy/Combined_Worldwide_Taxonomy_Synonyms.csv", float_format="%.0f", index=False)
 # Write the filtered DataFrame to a new csv
 merged_df.to_csv("Taxonomy/Combined_Worldwide_Taxonomy.csv", float_format="%.0f", index=False)
+
 
 
