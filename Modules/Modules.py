e3acde845cb453df68b9002e7b50129f6a41a076
Michael Walter <104144492+MichaelPWalter@users.noreply.github.com>
2023-03-11 13:47:39 +0100

    Create Modules.py

---
+++ Modules/Modules.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def update_pip():
+    subprocess.check_call(['pip','install','--upgrade','pip'])
+
+def install(package):
+    subprocess.check_call(['pip', 'install', package])
+
+update_pip()
+
+# Replace 'requests' with any other package you want to install
+install('requests')
