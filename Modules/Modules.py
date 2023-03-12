6b4109540fe2777b2a1febf3f91e64b219b2f66f
Michael Walter <104144492+MichaelPWalter@users.noreply.github.com>
2023-03-13 00:02:17 +0100

    Update Modules.py

--- Modules/Modules.py
+++ Modules/Modules.py
@@ -3,12 +3,19 @@ import subprocess
 def update_pip():
     subprocess.check_call(['pip','install','--upgrade','pip'])
 
-def install(package):
-    subprocess.check_call(['pip', 'install', package])
+def install(Packages):
+    for package in Packages:
+        subprocess.check_call(['pip', 'install', package ])
 
 update_pip()
 
 # Replace 'requests' with any other package you want to install
-install('requests')
-install("pandas")
-install("rasterio")
+Packages = [
+"requests",
+"pandas",
+#"rasterio",
+#"matplotlib",
+"ipyleaflet"
+]
+
+install(Packages)
